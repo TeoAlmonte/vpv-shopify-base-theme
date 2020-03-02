4011b98216e717ee56496e35d73e245c6c444702
Teo Almonte <teoalmonte@gmail.com>
2020-03-02 16:58:30 -0500

    postcss comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
+/**
+ * PostCSS
+ * @desc How CSS will be processed. See webpack.common.js file to see where postcss-loader is
+ * being used.
+ * Autoprefixer = Adds vendor prefixers https://github.com/postcss/autoprefixer
+ * postcssPresentEnv = Use modern CSS and include fallbacks https://preset-env.cssdb.org/features#stage-3
+ */
+
 const autoprefixer = require('autoprefixer');
 const postcssPresetEnv = require('postcss-preset-env');
 
